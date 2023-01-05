e2bf1c3a53c52cd3167b76d3ed04e791e571a54d
jojobaa <joycarvalho59@gmail.com>
2023-01-05 15:58:05 -0300

    fix: sign up routes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import users from "./routes/users.routes.js"
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(users);
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`Server running in port ${port}`));
--- src/models/signup.models.js
+++ src/models/signup.models.js
@@ -4,5 +4,6 @@ export const newUserSchema = joi.object({
   name: joi.string().min(3).required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
+  confirmPassword: joi.ref('password'),
   url: joi.string().uri().trim().required()
 });
