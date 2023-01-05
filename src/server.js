b6a35028020f9960ace60c0f26458055016d802b
Akoruu.Dev <akoruu.dev@gmail.com>
2023-01-05 19:55:01 -0300

    FEAT [Connect]: Create database file; install express; update server file

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// tools
+import express from "express";
+import dotenv from "dotenv";
+
+// route path
+import users from './routes/users.routes.js';
+import signup from './routes/signup.routes.js';
+
+// config
+const server = express();
+server.use(express.json());
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+// routes
+server.use(users);
+server.use(signup);
+
+// listen
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
