e657b73e7f4d154a757c091c526194a695295d0a
jojobaa <joycarvalho59@gmail.com>
2023-01-06 00:16:24 -0300

    temp: test bugs

--- src/repositories/signup.repositories.js
+++ src/repositories/signup.repositories.js
@@ -1,11 +1,11 @@
 import { connection } from "../database/database.js"
 
 export function selectEmail(email) {
-    return connection.query("SELECT * FROM users WHERE email=$1", [email]);
+    return connection.query("SELECT * FROM public.users WHERE email=$1", [email]);
 }
 
-export function registerUser(name, email, hashedPassword) {
+export function registerUser(name, email, hashedPassword, url) {
     return connection.query(
-        `INSERT INTO users (name, email, password, url) VALUES ($1, $2, $3, $4);`, [name, email, hashedPassword, url]
+        `INSERT INTO public.users (name, email, password, url) VALUES ($1, $2, $3, #4);`, [name, email, hashedPassword, url]
     );
 }
