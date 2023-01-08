de6768cdc2732a4434aaed68abf68cc15298d48a
Akoruu.Dev <akoruu.dev@gmail.com>
2023-01-08 17:15:41 -0300

    FEAT [Date]: Update createDate function using dayjs library

--- src/models/createDate.models.js
+++ src/models/createDate.models.js
@@ -1,17 +1,14 @@
-import db from "../database/database.js";
+import dayjs from "dayjs";
+import { db } from "../database/database.js";
 
 export async function generateDate () {
-    const weekNum = date.week();
-    const hour = String(date.getHours()).length = 1 ? `0${date.getHours()}` : date.getHours();
-    const minutes = String(date.getMinutes()).length = 1 ? `0${date.getMinutes()}` : date.getMinutes();
-    const seconds = String(date.getSeconds()).length = 1 ? `0${date.getSeconds()}` : date.getSeconds();
+    const date = dayjs();
 
-    const date = new Date();
-    const day = date.getDay() + 1;
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const week = weekday(weekNum);
-    const time = `${hour}:${minutes}:${seconds}`
+    const day = Number(date.format('DD'));
+    const month = Number(date.format('MM'));
+    const year = Number(date.format('YYYY'));
+    const week = date.format('dddd');
+    const time = date.format(`HH:mm:ss`);
 
     await db.query(`INSERT INTO dates (day, month, year, week, time) VALUES ($1, $2, $3, $4, $5);`, [day, month, year, week, time]);
     const dates = await db.query(`SELECT id FROM dates ORDER BY id DESC`)
