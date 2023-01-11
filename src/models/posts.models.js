282709614e2319954670737e57e4e37eeb6d5fd7
HugoNicolau <nicolau.hugogiles@gmail.com>
2023-01-11 05:47:58 -0300

    Feat: Allowing null/empty description

--- src/models/posts.models.js
+++ src/models/posts.models.js
@@ -3,5 +3,5 @@ import joi from "joi";
 export const postSchema = joi.object({
     userId: joi.number().required(),
     link: joi.string().uri().required(),
-    description: joi.string().min(3).required()
+    description: joi.string().min(3).allow(null).allow('')
 })
